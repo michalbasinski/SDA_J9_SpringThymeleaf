7c72640a60fa044e2d6e0c694deef48a8a14bf89
Michał Basiński <michal.basinski88@gmail.com>
2018-12-15 11:39:55 +0100

    form

--- src/main/java/pl/sda/gui/demo/BookController.java
+++ src/main/java/pl/sda/gui/demo/BookController.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,8 +37,30 @@ public class BookController {
                 .map(x -> new BookDto(x.getTitle(), x.getAuthor(), x.getIsbn()))
                 .collect(Collectors.toList());
 
-        model.addAttribute("books", bookDtos);
+        model.addAttribute("booksDtos", bookDtos);
         return "books";
+    }
+
+    @RequestMapping("/addBook")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new BookDto());
+        return "addBook";
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.POST)
+    public String addBook(@ModelAttribute BookDto bookDto, Model model) {
+        Book book = new Book(null, bookDto.getTitle(), bookDto.getAuthor(), bookDto.getIsbn());
+
+        bookRepository.save(book);
+
+        List<Book> books = bookRepository.findAll();
+        List<BookDto> bookDtos = books.stream()
+                .map(x -> new BookDto(x.getTitle(), x.getAuthor(), x.getIsbn()))
+                .collect(Collectors.toList());
+
+        model.addAttribute("booksDtos", bookDtos);
+
+        return "/books";
     }
 
 }
